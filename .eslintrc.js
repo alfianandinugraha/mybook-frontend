4453a12199f84a571d1256ec75c3a7bff3da5a13
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-02 09:06:19 +0700

    chore(init): init app with create-react-typescript-app

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+
+const { NODE_ENV } = process.env
+
+const config = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*/**.ts'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json'],
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': NODE_ENV === 'development' ? 'off' : 'error',
+    'import/newline-after-import': 'error',
+    quotes: ['error', 'single'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    semi: 'off',
+    '@typescript-eslint/semi': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    '@typescript-eslint/comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+      },
+    ],
+    '@typescript-eslint/no-var-requires': ['off'],
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-unresolved': 'off',
+  },
+}
+
+module.exports = config
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = (): React.ReactElement => <p>Hello world ! 👋🏼👋🏼👋🏼</p>
+
+export default App
