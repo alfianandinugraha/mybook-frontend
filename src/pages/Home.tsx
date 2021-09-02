c4f563764ab75627a876ab724150b3a23cdb2d87
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-02 14:46:30 +0700

    feat(dummy items): create dummy items of book

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Button, Grid, Typography } from '@material-ui/core'
+import { Book } from 'ApiState'
+import BookItem from '@/components/BookItem'
+
+const generateBook = (
+  title: string,
+  description: string,
+  authors: string[]
+): Book => ({
+  id: Math.random().toString(),
+  userId: Math.random().toString(),
+  title,
+  description,
+  authors,
+})
+
+const books: Book[] = [
+  generateBook('Hello', 'Hello world', ['alfian', 'andi']),
+  generateBook('Hello', 'Hello world', ['alfian', 'andi']),
+  generateBook('Hello', 'Hello world', ['alfian', 'andi']),
+  generateBook('Hello', 'Hello world', ['alfian', 'andi']),
+  generateBook('Hello', 'Hello world', ['alfian', 'andi']),
+]
 
 const Home = (): React.ReactElement => (
-  <Grid container>
+  <Grid container direction="column">
     <Grid item>
       <Typography variant="h4">MyBooks</Typography>
+      <Typography>Welcome, Alfian</Typography>
+    </Grid>
+    <Grid item>
+      <Button color="primary" variant="contained">
+        Add
+      </Button>
+    </Grid>
+    <Grid item style={{ marginTop: '1rem' }}>
+      <Grid container spacing={3}>
+        {books.map((book) => (
+          <BookItem {...book} key={book.id} />
+        ))}
+      </Grid>
     </Grid>
   </Grid>
 )
