9b06d466d0573820d75a173b475647d7e8406c16
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-02 10:39:52 +0700

    feat(input handling): add input handling on login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Grid,
@@ -6,6 +6,7 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core'
+import { InputState } from 'ApiState'
 
 const useClasses = makeStyles(() => ({
   textField: {
@@ -14,7 +15,19 @@ const useClasses = makeStyles(() => ({
 }))
 
 const Login = (): React.ReactElement => {
+  const [email, setEmail] = useState<InputState>({
+    value: '',
+    errorMessage: '',
+  })
+  const [password, setPassword] = useState<InputState>({
+    value: '',
+    errorMessage: '',
+  })
   const classes = useClasses()
+
+  const submitForm = () => {
+    console.log(email, password)
+  }
 
   return (
     <Grid container direction="column" style={{ width: '25%' }}>
@@ -27,7 +40,16 @@ const Login = (): React.ReactElement => {
           label="Email"
           placeholder="your@email.com"
           type="email"
+          error={!!email.errorMessage}
+          helperText={email.errorMessage}
           className={classes.textField}
+          value={email.value}
+          onChange={(e) => {
+            setEmail({
+              ...email,
+              value: e.target.value,
+            })
+          }}
         />
       </Grid>
       <Grid item>
@@ -36,13 +58,22 @@ const Login = (): React.ReactElement => {
           label="Password"
           placeholder="********"
           type="password"
+          error={!!password.errorMessage}
+          helperText={password.errorMessage}
           className={classes.textField}
+          value={password.value}
+          onChange={(e) => {
+            setPassword({
+              ...email,
+              value: e.target.value,
+            })
+          }}
         />
       </Grid>
       <Grid item>
         <Grid container alignItems="center">
           <Grid item style={{ marginRight: '1rem' }}>
-            <Button color="primary" variant="contained">
+            <Button color="primary" variant="contained" onClick={submitForm}>
               Login
             </Button>
           </Grid>
