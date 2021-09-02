ef15057d008a85efe8da0f8a24b37bb84dfa1916
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-02 10:29:19 +0700

    feat(page): add design for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,58 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import {
+  Button,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography,
+} from '@material-ui/core'
 
-const Login = (): React.ReactElement => <Typography>Login</Typography>
+const useClasses = makeStyles(() => ({
+  textField: {
+    marginBottom: '1rem',
+  },
+}))
+
+const Login = (): React.ReactElement => {
+  const classes = useClasses()
+
+  return (
+    <Grid container direction="column" style={{ width: '25%' }}>
+      <Grid item>
+        <Typography variant="h4">Login</Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Email"
+          placeholder="your@email.com"
+          type="email"
+          className={classes.textField}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Password"
+          placeholder="********"
+          type="password"
+          className={classes.textField}
+        />
+      </Grid>
+      <Grid item>
+        <Grid container alignItems="center">
+          <Grid item style={{ marginRight: '1rem' }}>
+            <Button color="primary" variant="contained">
+              Login
+            </Button>
+          </Grid>
+          <Grid item>
+            <Typography style={{ cursor: 'pointer' }}>Register</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
 
 export default Login
