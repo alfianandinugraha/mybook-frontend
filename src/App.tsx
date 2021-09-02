a9c6f0f4e3e85281e90690c74405f83b19dfa1c0
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-02 12:56:50 +0700

    feat(page): init register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Login from '@/pages/Login'
+import Register from '@/pages/Register'
 import { Container } from '@material-ui/core'
 
 const App = (): React.ReactElement => (
@@ -8,6 +9,9 @@ const App = (): React.ReactElement => (
     <Switch>
       <Route path="/login" exact>
         <Login />
+      </Route>
+      <Route path="/register" exact>
+        <Register />
       </Route>
     </Switch>
   </Container>
---
+++ src/pages/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+
+const Register = (): React.ReactElement => (
+  <Grid container direction="column" style={{ width: '25%' }}>
+    <Grid item>
+      <Typography variant="h4">Register</Typography>
+    </Grid>
+  </Grid>
+)
+
+export default Register
