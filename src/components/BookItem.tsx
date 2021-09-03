f018cd7a0857d9261b8a66ccfd555748f9c6ec2b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-03 15:59:35 +0700

    feat(drawer): add BookDrawer on BookItem

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Book } from 'ApiState'
 import { Button, ButtonGroup, Grid, Typography } from '@material-ui/core'
+import BookDrawer from '@/components/BookDrawer'
 
 interface BookItemProps extends Book {}
 
 const BookItem = (props: BookItemProps): React.ReactElement => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const mapAuthor = props.authors.reduce((pre, curr) => `${pre}, ${curr}`)
 
   const deleteHandler = () => {
@@ -12,33 +14,45 @@ const BookItem = (props: BookItemProps): React.ReactElement => {
   }
 
   const updateHandler = () => {
-    console.log(`Updating ${props.id}`)
+    setIsDrawerOpen(true)
   }
 
   return (
-    <Grid item md={4}>
-      <Grid container direction="column">
-        <Grid item>
-          <Typography variant="h6">{props.title}</Typography>
-          <Typography>{props.description}</Typography>
-          <Typography variant="caption">{mapAuthor}</Typography>
-        </Grid>
-        <Grid item>
-          <ButtonGroup>
-            <Button
-              color="secondary"
-              variant="outlined"
-              onClick={deleteHandler}
-            >
-              Delete
-            </Button>
-            <Button color="primary" variant="outlined" onClick={updateHandler}>
-              Update
-            </Button>
-          </ButtonGroup>
+    <>
+      <Grid item md={4}>
+        <Grid container direction="column">
+          <Grid item>
+            <Typography variant="h6">{props.title}</Typography>
+            <Typography>{props.description}</Typography>
+            <Typography variant="caption">{mapAuthor}</Typography>
+          </Grid>
+          <Grid item>
+            <ButtonGroup>
+              <Button
+                color="secondary"
+                variant="outlined"
+                onClick={deleteHandler}
+              >
+                Delete
+              </Button>
+              <Button
+                color="primary"
+                variant="outlined"
+                onClick={updateHandler}
+              >
+                Update
+              </Button>
+            </ButtonGroup>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <BookDrawer
+        open={isDrawerOpen}
+        type="UPDATE"
+        onClose={() => setIsDrawerOpen(false)}
+        onClickClose={() => setIsDrawerOpen(false)}
+      />
+    </>
   )
 }
 
