6adb6dd95d6601a546afb0b44fd674f817723629
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-04 12:46:22 +0700

    feat(auth service): add register user

--- src/services/http/auth.ts
+++ src/services/http/auth.ts
@@ -1,4 +1,10 @@
-import { ApiResponse, LoginBody, LoginData } from 'HTTPApi'
+import {
+  ApiResponse,
+  LoginBody,
+  LoginData,
+  RegisterBody,
+  RegisterData,
+} from 'HTTPApi'
 import CookieService from '@/services/utils/cookie'
 import HttpService from '@/services/utils/http'
 
@@ -17,8 +23,22 @@ const login = async (email: string, password: string): Promise<void> => {
   }
 }
 
+const register = async (body: RegisterBody): Promise<void> => {
+  try {
+    const { data } = await HttpService.auth.post<ApiResponse<RegisterData>>(
+      '/register',
+      body
+    )
+    const { accessToken, refreshToken } = data.data
+    CookieService.setToken(accessToken, refreshToken)
+  } catch (err) {
+    throw err.response ? new Error(err.response.data.message) : err
+  }
+}
+
 const AuthService = {
   login,
+  register,
 }
 
 export default AuthService
