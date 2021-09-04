75d0d2210404c29f171f3b713653135c31d18317
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-04 16:04:43 +0700

    feat(http service): add http with interceptor for handle refresh token

--- src/services/utils/http.ts
+++ src/services/utils/http.ts
@@ -1,9 +1,39 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
+import CookieService from '@/services/utils/cookie'
+import AuthService from '@/services/http/auth'
 
-const httpAuth = { ...axios }
-httpAuth.defaults.baseURL = 'http://localhost:5500/api/auth/'
+const httpAuth = axios.create({
+  baseURL: 'http://localhost:5500/api/auth/',
+})
+
+const http = axios.create({
+  baseURL: 'http://localhost:5000/api/',
+})
+http.interceptors.request.use((config) => {
+  const newConfig = { ...config }
+  newConfig.headers.Authorization = `bearer ${CookieService.getAccessToken()}`
+  return newConfig
+})
+// reference : https://thedutchlab.com/blog/using-axios-interceptors-for-refreshing-your-api-token
+http.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  async (axiosErr) => {
+    try {
+      const newConfig = { ...axiosErr.config }
+      if (!newConfig._retry) {
+        newConfig._retry = true
+        await AuthService.requestAccessToken()
+        return await http(newConfig)
+      }
+    } catch (err) {
+      return Promise.reject(err)
+    }
+    return Promise.reject(axiosErr)
+  }
+)
 
 const HttpService = {
+  ...http,
   auth: httpAuth,
 }
 
