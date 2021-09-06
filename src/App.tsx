3ebc67fc43b28ddced3edde5d363c434346c56f5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-09-06 15:14:06 +0700

    feat(component): implement RouteGuard for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import { Container } from '@material-ui/core'
 import Home from '@/pages/Home'
+import { RouteGuard } from '@/hoc/Guard'
 
-const App = (): React.ReactElement => (
-  <Container maxWidth="md">
-    <Switch>
-      <Route path="/login" exact>
-        <Login />
-      </Route>
-      <Route path="/register" exact>
-        <Register />
-      </Route>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-    </Switch>
-  </Container>
-)
+const App = (): React.ReactElement => {
+  return (
+    <Container maxWidth="md">
+      <Switch>
+        <RouteGuard path="/login" exact>
+          <Login />
+        </RouteGuard>
+        <RouteGuard path="/register" exact>
+          <Register />
+        </RouteGuard>
+        <RouteGuard protected path="/" exact>
+          <Home />
+        </RouteGuard>
+      </Switch>
+    </Container>
+  )
+}
 
 export default App
